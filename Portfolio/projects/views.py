8acc59584aba187cb7db030b2f5512ff3ea2a104
MaxCurtis-creater <57647617+MaxCurtis-creater@users.noreply.github.com>
2023-12-22 19:13:00 -0700

    navbar update

--- Portfolio/projects/views.py
+++ Portfolio/projects/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = ''
-    return render(request=request, template_name='basic_html/index.html', context={'context': context})
+    return render(request=request, template_name='project_html/project.html', context={'context': context})
